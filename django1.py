d621f8c7a386346949612f1ccd2c5115516df79e
Bhavya Modh <bhavyamodh162@gmail.com>
2026-06-03 07:38:37 +0000

    add django1.py

--- django1.py
+++ django1.py
@@ -1,4 +1,13 @@
+import os
+import sys
 import time
+import django
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -7,11 +16,17 @@ from django.contrib.auth.models import User
 @receiver(post_save, sender=User)
 def slow_signal_receiver(sender, instance, created, **kwargs):
     print("Signal started...")
-    time.sleep(5)
+    time.sleep(5)  # This will block execution
     print("Signal finished!")
-
 
 def trigger_user_creation():
     print("Before creating user")
+    
+
+    User.objects.filter(username="testuser").delete()
+    
     User.objects.create_user(username="testuser")
     print("After creating user and signal finishes")
+
+if __name__ == "__main__":
+    trigger_user_creation()
