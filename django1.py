bea48d937df208777d3a814c191b260e19d5df61
Bhavya Modh <bhavyamodh162@gmail.com>
2026-06-03 06:41:21 +0000

    add django1.py

--- django1.py
+++ django1.py
@@ -3,14 +3,14 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
-# 1. The receiver function
+
 @receiver(post_save, sender=User)
 def slow_signal_receiver(sender, instance, created, **kwargs):
     print("Signal started...")
-    time.sleep(5) # This will block
+    time.sleep(5) 
     print("Signal finished!")
 
-# 2. Inside a view or shell script
+
 def trigger_user_creation():
     print("Before creating user")
     User.objects.create_user(username="testuser")
